712886c56620d2a03e2f2cdd3fe8bb2892977b8e
如果的如果 <admin@fosuss.com>
2020-09-04 10:34:22 +0800

    废除多维数组支持

--- src/Cache.php
+++ src/Cache.php
@@ -848,9 +848,6 @@ class Cache
      */
     private function generateSocket($key): string
     {
-        // 当以多维路径作为key的时候，以第一个路径为主
-        $list = explode('.', $key);
-        $key = array_shift($list);
         $index = (base_convert(substr(md5($key), 0, 2), 16, 10) % $this->processNum) + 1;
         return $this->generateSocketByIndex($index);
     }

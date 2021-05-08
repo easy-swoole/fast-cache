95f5f2bdb22941b61acee8e60693efb0f798a387
gaobinzhan <gaobinzhan@gmail.com>
2021-05-08 13:43:47 +0800

    Fix: tests.

--- tests/process.php
+++ tests/process.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use EasySwoole\FastCache\Cache;
 use EasySwoole\FastCache\CacheProcessConfig;
@@ -9,26 +9,27 @@ use EasySwoole\Utility\File;
 
 // 设置落地
 Cache::getInstance()->setTickInterval(5 * 1000);//设置定时频率
+Cache::getInstance()->setTempDir(__DIR__ . '/../');
 Cache::getInstance()->setOnTick(function (SyncData $SyncData, CacheProcessConfig $cacheProcessConfig) {
     $data = [
-        'data'  => $SyncData->getArray(),
+        'data' => $SyncData->getArray(),
         'queue' => $SyncData->getQueueArray(),
-        'ttl'   => $SyncData->getTtlKeys(),
+        'ttl' => $SyncData->getTtlKeys(),
         // queue支持
-        'jobIds'     => $SyncData->getJobIds(),
-        'readyJob'   => $SyncData->getReadyJob(),
+        'jobIds' => $SyncData->getJobIds(),
+        'readyJob' => $SyncData->getReadyJob(),
         'reserveJob' => $SyncData->getReserveJob(),
-        'delayJob'   => $SyncData->getDelayJob(),
-        'buryJob'    => $SyncData->getBuryJob(),
+        'delayJob' => $SyncData->getDelayJob(),
+        'buryJob' => $SyncData->getBuryJob(),
     ];
     $path = __DIR__ . '/FastCacheData/' . $cacheProcessConfig->getProcessName();
-    File::createFile($path,serialize($data));
+    File::createFile($path, serialize($data));
 });
 
 // 启动时将存回的文件重新写入
 Cache::getInstance()->setOnStart(function (CacheProcessConfig $cacheProcessConfig) {
     $path = __DIR__ . '/FastCacheData/' . $cacheProcessConfig->getProcessName();
-    if(is_file($path)){
+    if (is_file($path)) {
         $data = unserialize(file_get_contents($path));
         $syncData = new SyncData();
         $syncData->setArray($data['data']);
@@ -47,26 +48,26 @@ Cache::getInstance()->setOnStart(function (CacheProcessConfig $cacheProcessConfi
 // 在守护进程时,php easyswoole stop 时会调用,落地数据
 Cache::getInstance()->setOnShutdown(function (SyncData $SyncData, CacheProcessConfig $cacheProcessConfig) {
     $data = [
-        'data'  => $SyncData->getArray(),
+        'data' => $SyncData->getArray(),
         'queue' => $SyncData->getQueueArray(),
-        'ttl'   => $SyncData->getTtlKeys(),
+        'ttl' => $SyncData->getTtlKeys(),
         // queue支持
-        'jobIds'     => $SyncData->getJobIds(),
-        'readyJob'   => $SyncData->getReadyJob(),
+        'jobIds' => $SyncData->getJobIds(),
+        'readyJob' => $SyncData->getReadyJob(),
         'reserveJob' => $SyncData->getReserveJob(),
-        'delayJob'   => $SyncData->getDelayJob(),
-        'buryJob'    => $SyncData->getBuryJob(),
+        'delayJob' => $SyncData->getDelayJob(),
+        'buryJob' => $SyncData->getBuryJob(),
     ];
     $path = __DIR__ . '/FastCacheData/' . $cacheProcessConfig->getProcessName();
-    File::createFile($path,serialize($data));
+    File::createFile($path, serialize($data));
 });
 
 $processes = Cache::getInstance()->initProcess();
 
-foreach ($processes as $process){
+foreach ($processes as $process) {
     $process->getProcess()->start();
 }
 
-while($ret = \Swoole\Process::wait()) {
+while ($ret = \Swoole\Process::wait()) {
     echo "PID={$ret['pid']}\n";
 }
